c2fd8ce7aee68575cd1b6869888bf5e45f0fd0ab
maeda-y42 <mobiusloopnoah95@ezweb.ne.jp>
2019-07-01 20:57:00 +0900

    2019/7/1

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -5,9 +5,41 @@ public class Test {
 		Person taro= new Person();
 		taro.name="山田太郎";
 		taro.age=20;
+		taro.phoneNumber="03-";
+		taro.address="東京都";
+
+
 
 		System.out.println(taro.name);
 		System.out.println(taro.age);
-	}
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
+		taro.talk();
+		taro.walk();
+		taro.run();
 
+
+
+
+		Robot aibo = new Robot();
+		aibo.name = "aibo";
+
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
+
+		Robot asimo = new Robot();
+		asimo.name = "asimo";
+
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
+
+		Robot pepper = new Robot();
+		pepper.name = "pepper";
+
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
+	}
 }
