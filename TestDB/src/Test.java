24dc237a4e911d9d1cfb0af82074485102db6191
maeda-y42 <mobiusloopnoah95@ezweb.ne.jp>
2019-10-21 21:56:00 +0900

    2019/10/21

--- TestDB/src/Test.java
+++ TestDB/src/Test.java
@@ -3,6 +3,6 @@ public class Test {
 
 	public static void main(String[] args){
 		TestUserDAO dao = new TestUserDAO();
-		dao.selectByPassword("456");
+		dao.delete("jiro");
 	}
 }
--- TestDB/src/TestUserDAO.java
+++ TestDB/src/TestUserDAO.java
@@ -1,34 +1,32 @@
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class TestUserDAO{
+public class TestUserDAO {
 
-	String name ="";
-	String password = "";
+    String name = "";
+    String password = "";
 
-	public void updateUserName(String oldName,String newName){
-		DBConnector db = new DBConnector();
-		Connection con = db.getConnection();
+    public void delete(String name) {
+        DBConnector db = new DBConnector();
+        Connection con = db.getConnection();
 
-	String sql = "update test_table set user_name=? where";
-	try{
-		PreparedStatement ps = con.prepareStatement(sql);
-		ps.setString(1,password);
-		ResultSet rs = ps.executeQuery();
-		while(rs.next()){
-			System.out.println(rs.getString("user_name"));
-			System.out.println(rs.getString("password"));
-		}
-	}catch(SQLException e){
-		e.printStackTrace();
-	}
-	try{
-		con.close();
-	}catch(SQLException e){
-		e.printStackTrace();
-	}
-	}
+        String sql = "delete from test_table where user_name=?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, name);
+            int i = ps.executeUpdate();
+            if (i > 0) {
+                System.out.println(i + "件削除されました");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
